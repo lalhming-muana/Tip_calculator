206b3a20a707490ac52cb89535384fe5a8814b0f
lhmuana <edd_vai@yahoo.co.in>
2023-09-19 22:53:09 +0530

    The state composable part is completed

--- app/src/main/java/com/example/tiptime/MainActivity.kt
+++ app/src/main/java/com/example/tiptime/MainActivity.kt
@@ -22,12 +22,19 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
@@ -54,6 +61,16 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
+fun EditNumberField(modifier: Modifier = Modifier)
+{
+    var amountInput by remember {mutableStateOf("")}
+    TextField(value = amountInput,
+              onValueChange ={amountInput = it},
+              modifier = modifier  )
+}
+
+
+@Composable
 fun TipTimeLayout() {
     Column(
         modifier = Modifier.padding(40.dp),
@@ -66,6 +83,9 @@ fun TipTimeLayout() {
                 .padding(bottom = 16.dp)
                 .align(alignment = Alignment.Start)
         )
+
+        EditNumberField(modifier = Modifier.padding(bottom =32.dp).fillMaxWidth())
+
         Text(
             text = stringResource(R.string.tip_amount, "$0.00"),
             style = MaterialTheme.typography.displaySmall
